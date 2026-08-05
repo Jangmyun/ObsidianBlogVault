9f917459a2d8e22485a0aecad52c3c01b06b8296
Jangmyun <cndlsrb2739@gmail.com>
2026-08-05 22:33:17 +0900

    SEO: pageTitleSuffix 설정해 페이지 타이틀에 사이트명 노출

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Jangmyun's Vault",
-    pageTitleSuffix: "",
+    pageTitleSuffix: " · Jangmyun's Vault",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
